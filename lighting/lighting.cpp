d207e90092fd5731f6c907fe1b6059875a4ba04c
cameronnathan707 <62361875+cameronnathan707@users.noreply.github.com>
2021-01-17 22:51:12 -0600

    Removed unnecesary test code

--- lighting/lighting.cpp
+++ lighting/lighting.cpp
@@ -13,36 +13,7 @@
 
 using namespace std;
 string colour;
-void choices(int a){
 
-	if (a == 1) {
-		//purple blue
-		colour = "purple/blue";
-	}
-	else if (a == 2) {
-		//green blue
-		colour = "green/blue";
-	}
-	else if (a == 3) {   
-		//red purple
-		colour = "red/purple";
-	}
-	else if (a == 4) {
-		//white
-		colour = "white";
-	}
-	else if (a == 5) {
-		//rainbow wave
-		colour = "rainbow wave";
-	}
-}
 int main( int argc, char* argv[]) {
 	string colourSelection = argv[2];
-	ofstream myfile;
-    choices(stoi(colourSelection));
-	myfile.open("C:/Users/camer/Desktop/test.txt");
-	myfile << "Write colour to file: " + colourSelection+"\n"+colour;
-	myfile.close();
-	cout << colour;
-    return 0;
 }
