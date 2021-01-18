918575b696fff9e1e267ce3eda88c003ad569282
cameronnathan707 <62361875+cameronnathan707@users.noreply.github.com>
2021-01-17 22:50:21 -0600

    Added required library includes

--- lighting/lighting.cpp
+++ lighting/lighting.cpp
@@ -1,9 +1,16 @@
-
 //Lighting_Editor
 //This is a code to recive input from autohotkey
+
 #include <fstream>
 #include <iostream>
 #include <string>
+#include "C:\Program Files\Alienware\Alienware Command Center\AlienFX SDK\includes\LFX2.h"
+
+#include <ctime>
+#include <iomanip>
+#include <Windows.h>
+#include <tchar.h>
+
 using namespace std;
 string colour;
 void choices(int a){
