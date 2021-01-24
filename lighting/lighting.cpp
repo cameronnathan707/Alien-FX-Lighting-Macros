12892cb9dda5a554b00caf5473cb4c63cb8eb176
cameronnathan707 <62361875+cameronnathan707@users.noreply.github.com>
2021-01-23 21:38:27 -0600

    completed new function definitions & implementations for seperate lighting zones

--- lighting/lighting.cpp
+++ lighting/lighting.cpp
@@ -11,35 +11,36 @@
 #include <Windows.h>
 #include <tchar.h>
 
-// SDKSample.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+using namespace std;
+string colour;
 
-#include <ctime>
-#include <iostream>
-#include <iomanip>
-#include <Windows.h>
-#include <tchar.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
+int main(int argc, char *argv[])
 {
+	#define LFX_PURPLE {255, 0, 255, 255};
+	#define LFX_BLUE {59,0,255,255};
+	#define LFX_TEAL {0,255,212,255};
+	#define LFX_RED {255,0,0,255}
+	#define LFX_LIGHT_GREEN {0,255,24,255};
+	#define LFX_WHITE {233,244,248,255};
+
+	string colourSelection = argv[2];
 	LFX2INITIALIZE initFunction;
 	LFX2RELEASE releaseFunction;
-	
+
 	LFX2GETNUMDEVICES getNumDevicesFunction;
 	LFX2GETDEVDESC getDeviceDescriptionFunction;
 	LFX2GETNUMLIGHTS getNumLightsFunction;
 	LFX2GETLIGHTDESC getLightDescriptionFunction;
+
+	LFX2SETLIGHTACTIONCOLOR setLightActionColor;
 	LFX2GETLIGHTLOC getLightLocationFunction;
-
 	LFX2SETLIGHTCOL setLightColor;
-
+	LFX2SETTIMING setTiming;
 	LFX2RESET resetFunction;
 	LFX2UPDATE updateFunction;
 	LFX2LIGHT lightFunction;
 	LFX2GETVERSION versionFunction;
-	LFX_COLOR lights[] = {{181,7,213,100}, {250,2,2,100}, {2,2,250,100}, {2,250,2,100},{181,7,213,100}, {250,2,2,100}, {2,2,250,100}, {2,250,2,100} ,{181,7,213,100}, {250,2,2,100}, {2,2,250,100}, {2,250,2,100} ,{181,7,213,100}, {250,2,2,100}, {2,2,250,100}, {2,250,2,100} ,{181,7,213,100}, {250,2,2,100}, {2,2,250,100}, {2,250,2,100}  };
+		
 	HINSTANCE hLibrary = LoadLibrary(_T(LFX_DLL_NAME));
 	if (hLibrary)
 	{
@@ -50,10 +51,11 @@ int main()
 		getDeviceDescriptionFunction = (LFX2GETDEVDESC)GetProcAddress(hLibrary, LFX_DLL_GETDEVDESC);
 		getNumLightsFunction = (LFX2GETNUMLIGHTS)GetProcAddress(hLibrary, LFX_DLL_GETNUMLIGHTS);
 		getLightDescriptionFunction = (LFX2GETLIGHTDESC)GetProcAddress(hLibrary, LFX_DLL_GETLIGHTDESC);
+
 		getLightLocationFunction = (LFX2GETLIGHTLOC)GetProcAddress(hLibrary, LFX_DLL_GETLIGHTLOC);
-
-
 		setLightColor = (LFX2SETLIGHTCOL)GetProcAddress(hLibrary, LFX_DLL_SETLIGHTCOL);
+		setLightActionColor =(LFX2SETLIGHTACTIONCOLOR)GetProcAddress(hLibrary,LFX_DLL_SETLIGHTACTIONCOLOR);
+		setTiming=(LFX2SETTIMING)GetProcAddress(hLibrary,LFX_DLL_SETTIMING);
 		resetFunction = (LFX2RESET)GetProcAddress(hLibrary, LFX_DLL_RESET);
 		updateFunction = (LFX2UPDATE)GetProcAddress(hLibrary, LFX_DLL_UPDATE);
 		lightFunction = (LFX2LIGHT)GetProcAddress(hLibrary, LFX_DLL_LIGHT);
@@ -64,71 +66,298 @@ int main()
 		{
 			if (versionFunction == NULL)
 				std::cout << "SDK version not found ..." << std::endl;
-			else
+
+			resetFunction();
+
+			int color = LFX_GREEN;
+			lightFunction(LFX_ALL, LFX_OFF | LFX_FULL_BRIGHTNESS);
+			updateFunction();
+			system("cls");
+			std::cout << "Color: " << std::hex << std::setw(6) << std::setfill('0') << color << std::endl;
+			Sleep(100);
+
+			LFX_COLOR purple = LFX_PURPLE;
+			LFX_COLOR blue = LFX_BLUE;
+			LFX_COLOR teal = LFX_TEAL;
+			LFX_COLOR red = LFX_RED;
+			LFX_COLOR light_green = LFX_LIGHT_GREEN;
+			LFX_COLOR white = LFX_WHITE;
+			
+			switch (stoi(colourSelection))
 			{
-				unsigned int versionSize = 255;
-				char* version = new char[versionSize];
-				result = versionFunction(version, versionSize);
-				std::cout << "SDK version: " << version << std::endl;
-				delete[]version;
-			}
+			case 1:
+				//purple blue
+				colour = "purple/blue";
+				//setTiming(10000);
+				//updateFunction();
+				//Sleep(100);
+				setLightColor(0, 1, &purple);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 2, &purple);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 3, &purple);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 4, &purple);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 5, &blue);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 6, &blue);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 7, &blue);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 8, &blue);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 9, &purple);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 10, &purple);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 11, &purple);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 12, &purple);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 13, &blue);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 14, &blue);
+				updateFunction();	
+				setTiming(5000);
+				updateFunction();
+				Sleep(100);
+				setLightActionColor(0, 15,LFX_ACTION_PULSE,&purple);
+				updateFunction();
+				Sleep(100);
+				
 
-			unsigned int numDevices = 0;
-			result = getNumDevicesFunction(&numDevices);
-			std::cout << "Devices: " << numDevices << std::endl;
+				system("cls");
+				std::cout << "Color: " << std::hex << std::setw(6) << std::setfill('0') << color << std::endl;
+				break;
+			case 2:
+				//green teal
+				colour = "green/blue";
 
-			for (unsigned int devIndex = 0; devIndex < numDevices; devIndex++)
-			{
-				unsigned int numLights = 0;
-				unsigned char devType = 0;
-				unsigned char* devTypePtr = &devType;
+				setLightColor(0, 1, &light_green);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 2, &light_green);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 3, &light_green);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 4, &light_green);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 5, &teal);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 6, &teal);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 7, &teal);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 8, &teal);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 9, &light_green);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 10, &light_green);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 11, &light_green);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 12, &light_green);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 13, &teal);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 14, &teal);	
+				updateFunction();
+				Sleep(100);	
+				setLightColor(0, 15, &light_green);
+				updateFunction();
+				Sleep(100);
+				system("cls");
+				std::cout << "Color: " << std::hex << std::setw(6) << std::setfill('0') << color << std::endl;
+				break;
+			case 3:
+				//red purple
+				colour = "red/purple";
 
-				unsigned int descSize = 255;
-				char* description = new char[descSize];
-				result = getDeviceDescriptionFunction(devIndex, description, descSize, &devType);
-				std::cout << "Description: " << description << std::endl;
-				delete[]description;
-
-				description = new char[descSize];
-				result = getNumLightsFunction(devIndex, &numLights);
-				for (unsigned int lightIndex = 0; lightIndex < numLights; lightIndex++)
-				{
-					result = getLightDescriptionFunction(devIndex, lightIndex, description, descSize);
-					if (result != LFX_SUCCESS)
-						continue;
-					// LFX_POSITION* pos = new LFX_POSITION;
-					// LFX_POSITION* pos1 = new LFX_POSITION;
-
-					// LFX_RESULT result = getLightLocationFunction(0, 1, pos);
-					// LFX_RESULT result2 = getLightLocationFunction(0, 2, pos1);
-					LFX_COLOR col = lights[lightIndex];
-					LFX_RESULT result3 = setLightColor(devIndex, lightIndex, &col);
-					updateFunction();
-					Sleep(100);
-					std::cout << "\tLight: " << lightIndex << "\tDescription: " << description << std::endl;
-					// printf("Value:  %d\n", *pos );	
-					// printf("Value:  %d\n", *pos1 );	
-					// delete pos;
-				}
-
-				delete[]description;
-			}
-
-			Sleep(1000);
-			//resetFunction();
-			/*
-			for (int i = 0; i <= 0; i++)
-			{
-				int color = ((0xFF - i) << 16) | i;
-				lightFunction(LFX_ALL, color | LFX_FULL_BRIGHTNESS);
+				setLightColor(0, 1, &purple);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 2, &purple);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 3, &purple);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 4, &purple);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 5, &red);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 6, &red);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 7, &red);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 8, &red);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 9, &purple);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 10, &purple);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 11, &purple);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 12, &purple);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 13, &red);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 14, &red);	
+				updateFunction();
+				Sleep(100);	
+				setLightColor(0, 15, &purple);
+				updateFunction();
+				Sleep(100);
+				system("cls");
+				std::cout << "Color: " << std::hex << std::setw(6) << std::setfill('0') << color << std::endl;
+				break;
+			case 4:
+				//white
+				colour = "white";
+				
+				setLightColor(0, 1, &white);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 2, &white);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 3, &white);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 4, &white);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 5, &white);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 6, &white);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 7, &white);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 8, &white);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 9, &white);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 10, &white);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 11, &white);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 12, &white);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 13, &white);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 14, &white);	
+				updateFunction();
+				Sleep(100);	
+				setLightColor(0, 15, &white);	
+				updateFunction();
+				Sleep(100);			
 				updateFunction();
 				system("cls");
 				std::cout << "Color: " << std::hex << std::setw(6) << std::setfill('0') << color << std::endl;
+				break;
+			case 5:
+				//rainbow wave
+				colour = "rainbow wave";
+				setLightColor(0, 1, &red);
+				updateFunction();
 				Sleep(100);
+				setLightColor(0, 2, &light_green);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 3, &blue);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 4, &purple);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 5, &purple);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 6, &blue);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 7, &light_green);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 8, &red);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 9, &light_green);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 10, &blue);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 11, &purple);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 12, &red);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 13, &light_green);
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 14, &blue);		
+				updateFunction();
+				Sleep(100);
+				setLightColor(0, 15, &purple);	
+				Sleep(100);
+				updateFunction();
+				system("cls");
+				std::cout << "Color: " << std::hex << std::setw(6) << std::setfill('0') << color << std::endl;
+				break;
 			}
-			*/
-			std::cout << "Done.\r\n\r\nPress any key to finish ..." << std::endl;
-			//_gettch();
+			ofstream myfile;
+			myfile.open("C:/Users/camer/Desktop/test.txt");
+			myfile << "Write colour to file: " + colourSelection + "\n" + colour;
+			myfile.close();
+			cout << colour;
+
 			result = releaseFunction();
 		}
 		else
@@ -143,22 +372,9 @@ int main()
 				break;
 			}
 		}
-
 		FreeLibrary(hLibrary);
 	}
 	else
 		std::cout << "Failed to load LightFX.dll." << std::endl;
-
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
